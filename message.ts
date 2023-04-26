4b4f27f2d728357640dd3d4f0b5e9420ab7ddaa6
Faith Nina Marie Magsael <124368100+fnmrie@users.noreply.github.com>
2023-04-26 02:09:48 -0700

    Update message.ts

--- message.ts
+++ message.ts
@@ -146,145 +146,135 @@ abstract class Resort implements ResortEstablishments {
 // Manang Peyt
 class Hotel extends Resort {
 
-  public rooms: number;
-  public hotelPayments: string; 
-  public numOfDays: number;
+public rooms: number;
+public hotelPayments: string; 
+public numOfDays: number;
 
-  constructor (guestName: string,
-    guestAge: number,
-    telephoneNumber: string,
-    totalGuests: number,
-    identification: boolean,
-    withPets: boolean,
-    withTransportation: boolean,
-    withProhibitedObjects: boolean,
-    paymentMethod: string,
-    withReservation: boolean,
-    pwd: boolean,
-    SeatingCapacity: number,
-    ParkingLotCapacity: number,
-    //additional attributes
-    rooms: number, 
-    hotelPayments: string,
-    numOfDays: number){
-    super(guestName,
-      guestAge,
-      telephoneNumber,
-      totalGuests,
-      identification,
-      withPets,
-      withTransportation,
-      withProhibitedObjects,
-      paymentMethod,
-      withReservation,
-      pwd,
-      SeatingCapacity,
-      ParkingLotCapacity)
-    this.rooms = rooms;
-    this.hotelPayments = hotelPayments;
-    this.numOfDays = numOfDays;
+constructor (guestName: string,
+  guestAge: number,
+  telephoneNumber: string,
+  totalGuests: number,
+  identification: boolean,
+  withPets: boolean,
+  withTransportation: boolean,
+  withProhibitedObjects: boolean,
+  paymentMethod: string,
+  withReservation: boolean,
+  pwd: boolean,
+  SeatingCapacity: number,
+  ParkingLotCapacity: number,
+  //additional attributes
+  rooms: number, 
+  hotelPayments: string,
+  numOfDays: number){
+  super(guestName,
+    guestAge,
+    telephoneNumber,
+    totalGuests,
+    identification,
+    withPets,
+    withTransportation,
+    withProhibitedObjects,
+    paymentMethod,
+    withReservation,
+    pwd,
+    SeatingCapacity,
+    ParkingLotCapacity)
+  this.rooms = rooms;
+  this.hotelPayments = hotelPayments;
+  this.numOfDays = numOfDays;
+}
+
+
+public checkIn(): string {
+  if (this.withProhibitedObjects === true) {
+    return(`Sorry we cannot check you in!`);
+  } else {
+    this.rooms -= this.totalGuests;
+  return(`${this.guestName} successfullly checked in!`);
   }
+}
+
+public checkOut(): string {
+  this.rooms += this.totalGuests;
+  return(`${this.guestName} successfullly checked out!`)
+}
+
+public confirmReservation(): void {
+  this.withReservation === true ? (this.rooms-this.totalGuests)
+  : this.rooms;
+  console.log(`Reservation for ${this.guestName} has been confirmed.`)
+}
+
+public cancelReservation(): void {
+  this.withReservation === true ? (this.rooms + this.totalGuests)
+  : this.rooms;
+  console.log(`Reservation for ${this.guestName} has been cancelled.`)
+}
+
+public checkPaymentMethod(): string {
+    return (`We accept payments thru ${this.hotelPayments}. Thank You!`)
+ }
+
+public checkForDiscount():number {
+  let discount: number = 0;
+  if (this.pwd === true) {
+    discount = 0.45
+  } else if (this.guestAge <= 5 || this.guestAge >= 60) {
+    discount = 0.50
+  } else {
+    discount = 0
+  }
+  return discount;
+}
+
+private calculateBill(days = this.numOfDays): number {
+  let bill: number =  days * 1000;
+  let discount: number = bill * this.checkForDiscount();
+  let totalBill: number = bill - discount;
+  return totalBill;
+}
+
+public generateReceipt(): string {
+  let bill: number =  this.numOfDays * 1000;
+  let discount: number = bill * this.checkForDiscount();
+  return (`
+  Guest Name: ${this.guestName}
+  Subtotal: ${bill} Php
+  Discount: ${discount} Php
   
+  TOTAL: ${this.calculateBill()} Php`)
+}
 
-  public checkIn(): string {
-    if (this.withProhibitedObjects === true) {
-      return(`Sorry we cannot check you in!`);
-    } else {
-      this.rooms -= this.totalGuests;
-    return(`${this.guestName} successfullly checked in!`);
-    }
+public billOut(TotalMoney: number): string {
+  if (TotalMoney === this.calculateBill()) {
+    return (`Your payment of ${TotalMoney} Php was succesful.`)
+  } else if (TotalMoney > this.calculateBill()) {
+    let change: number = TotalMoney - this.calculateBill()
+    return (`Your change is ${change} Php.`)
+  } else {
+    let balance: number = this.calculateBill() - TotalMoney
+    return (`Your balance is ${balance}.`)
   }
+}
 
-  public checkOut(): string {
-    this.rooms += this.totalGuests;
-    return(`${this.guestName} successfullly checked out!`)
-  }
+public greetGuest(): string {
+  return (`Welcome to our hotel. Please enjoy your stay!`)
+}
 
-  public confirmReservation(): void {
-    this.withReservation === true ? (this.rooms-this.totalGuests)
-    : this.rooms;
-    console.log(`Reservation for ${this.guestName} has been confirmed.`)
-  }
+public getGuestInfo(): string {
+  return (`
+  Guest Name: ${this.guestName}
+  Contact Number: ${this.telephoneNumber}`)
+}
 
-  public cancelReservation(): void {
-    this.withReservation === true ? (this.rooms + this.totalGuests)
-    : this.rooms;
-    console.log(`Reservation for ${this.guestName} has been cancelled.`)
+public specialPromo(code: string) {
+  if (code === "sanaolGABALIK") {
+    console.log("You received 50% discount for your next stay with us!")
+  } else {
+    console.log("Invalid code.")
   }
-
-  public checkPaymentMethod(): string {
-    if (this.paymentMethod.includes(this.hotelPayments)) {
-      return (`You can pay us though your ${this.paymentMethod}`)
-    } else {
-      return (`We accept payments thru ${this.hotelPayments}. Thank You!`)
-    }
-  }
-
-  public checkForDiscount():number {
-    let discount: number = 0;
-    if (this.guestAge <= 5 && this.guestAge >= 60) {
-      discount = 0.50
-      console.log(`50% discount.`)
-    } else if (this.guestAge >= 6 && this.guestAge >= 10){
-      discount = 0.25
-      console.log(`25% discount.`)
-    } else if (this.pwd === true) {
-      discount = 0.45
-      console.log(`45% discount.`)
-    } else {
-      discount = 0
-      console.log(`Not eligible for discount.`)
-    }
-    return discount;
-  }
-
-  private calculateBill(days = this.numOfDays): number {
-    let bill: number =  days * 1000;
-    let discount: number = bill * this.checkForDiscount();
-    let totalBill: number = bill - discount;
-    return totalBill;
-  }
-
-  public generateReceipt(): string {
-    let bill: number =  this.numOfDays * 1000;
-    let discount: number = bill * this.checkForDiscount();
-    return (`
-    Guest Name: ${this.guestName}
-    Subtotal: ${bill} Php
-    Discount: ${discount} Php
-    
-    TOTAL: ${this.calculateBill()} Php`)
-  }
-
-  public billOut(TotalMoney: number): string {
-    if (TotalMoney === this.calculateBill(this.numOfDays)) {
-      return (`Your payment of ${TotalMoney} Php was succesful.`)
-    } else if (TotalMoney > this.calculateBill(this.numOfDays)) {
-      let change: number = TotalMoney - this.calculateBill(this.numOfDays)
-      return (`Your change is ${change} Php.`)
-    } else {
-      let balance: number = this.calculateBill(this.numOfDays) - TotalMoney
-      return (`Your balance is ${balance}.`)
-    }
-  }
-
-  public greetGuest(): string {
-    return (`Welcome to our hotel. Please enjoy your stay!`)
-  }
-
-  public getGuestInfo(): string {
-    return (`
-    Guest Name: ${this.guestName}
-    Contact Number: ${this.telephoneNumber}`)
-  }
-  
-  public specialPromo(code: string) {
-    if (code === "sanaolGABALIK") {
-      console.log("You received 50% discount for your next stay with us!")
-    } else {
-      console.log("Invalid code.")
-    }
-  }
+}
 
 }
 
